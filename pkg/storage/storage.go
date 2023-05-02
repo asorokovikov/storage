0a4548aa8e7a4a1e13850dfc97c92bcdd6d0d7b7
Artem <asorokovikov@gmail.com>
2023-05-02 20:41:59 +0300

    Fix pkg

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,6 +1,6 @@
 package storage
 
-import "github.com/fcentury/storage/internal/storage"
+import "github.com/asorokovikov/storage/internal/storage"
 
 func NewStorage() *storage.Storage {
 	return storage.NewStorage()
